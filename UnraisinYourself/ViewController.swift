f909921f85774b1e42bf1ac8f3aac70f8dc695a5
Han Kim <hanniek00@gmail.com>
2023-07-20 16:18:00 -0500

    Create initial Xcode project

---
+++ UnraisinYourself/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnraisinYourself
+//
+//  Created by Hannie Kim on 7/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
